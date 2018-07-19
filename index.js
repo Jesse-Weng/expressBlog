03b1414e23f3511441a605a62012226aa5eb8cee
jesse <jxweng@gizwits.com>
2018-07-19 20:28:19 +0800

    '添加自动化测试'

--- index.js
+++ index.js
@@ -80,7 +80,11 @@ routes(app)
 //     ]
 // }))
 
-// 监听端口，启动程序
-app.listen(config.port, function () {
-    console.log(`${pkg.name} listening on port ${config.port}`)
-})
+if (module.parent) {
+    module.exports = app
+} else {
+    // 监听端口，启动程序
+    app.listen(config.port, function () {
+        console.log(`${pkg.name} listening on port ${config.port}`)
+    })
+}
