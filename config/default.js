1ff167cbee2a700ee3a3f1d8038aecf6c62a7276
jesse <jxweng@gizwits.com>
2018-12-19 16:29:07 +0800

    修改session配置

--- config/default.js
+++ config/default.js
@@ -1,8 +1,8 @@
 module.exports = {
     port: 3002,
     session: {
-        secret: 'nodeJsBlog',
-        key: 'nodeJsBlog',
+        secret: 'expressBlog',
+        key: 'expressBlog',
         maxAge: 2592000000
     },
     mongodb: 'mongodb://localhost:27017/expressBlog'
